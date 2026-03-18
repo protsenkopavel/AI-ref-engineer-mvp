3544dab93d77064c7da0a28c17cab1d61b9fb3d1
Pavel Protsenko <pavelprotsenko@outlook.com>
2026-03-19 01:16:50 +0300

    first commit

---
+++ src/main/java/net/protsenko/refrigeration/engineer/airefengineermvp/AiRefEngineerMvpApplication.java
@@ -0,0 +1,13 @@
+package net.protsenko.refrigeration.engineer.airefengineermvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRefEngineerMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRefEngineerMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/protsenko/refrigeration/engineer/airefengineermvp/AiRefEngineerMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.refrigeration.engineer.airefengineermvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRefEngineerMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
